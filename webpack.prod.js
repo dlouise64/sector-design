81a972e0102d3b59b5917007a6818dc648595f9d
dlouise64 <dana.iti.work@gmail.com>
2020-02-02 21:50:34 +1300

    remove css rule from common. add to dev. add minifier

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -9,5 +10,16 @@ module.exports = merge(common, {
     filename: "[name].[contentHash].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  }
 });

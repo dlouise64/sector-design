3820c661a80082d1d9f9c423cb9b47211397537d
dlouise64 <dana.iti.work@gmail.com>
2020-02-02 21:30:50 +1300

    added html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,6 +8,7 @@ module.exports = {
     filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {

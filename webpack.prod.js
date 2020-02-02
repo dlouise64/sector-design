42fc3f9b1ab6474514447f685a9c868cc268afaf
dlouise64 <dana.iti.work@gmail.com>
2020-02-02 21:35:49 +1300

    add config for dev and prod

---
+++ webpack.prod.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.[contentHash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss\$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};

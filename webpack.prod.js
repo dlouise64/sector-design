74aa95fb20897cb475c616897c667cf2eb741971
dlouise64 <dana.iti.work@gmail.com>
2020-02-02 21:53:11 +1300

    optimise and minify css

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,17 @@ const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "[name].[contentHash].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserWebpackPlugin()]
   },
   plugins: [
     new CleanWebpackPlugin(),

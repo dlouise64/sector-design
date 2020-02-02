c361aa179a5612645111cfd4b09992b68da81e19
dlouise64 <dana.iti.work@gmail.com>
2020-02-02 21:34:15 +1300

    use template

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,11 @@ module.exports = {
     filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
   module: {
     rules: [
       {
